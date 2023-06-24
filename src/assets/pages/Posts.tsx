8caec9bf9854251f7c713d376ef1bc94192d3bf7
mehdi_hattou <mehdi_hattou@carrefour.com>
2023-06-24 23:32:16 +0200

    add target value + type

--- src/assets/pages/Posts.tsx
+++ src/assets/pages/Posts.tsx
@@ -2,18 +2,27 @@ import { useEffect, useState } from "react";
 import Postslist from "../components/Postslist";
 import "./posts.css";
 
+export interface PostData {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
 const Posts: React.FC = () => {
-  const [allPosts, setAllPosts] = useState(null);
+  const [allPosts, setAllPosts] = useState<PostData[] | null>(null);
+  const [numberOfPosts, setNumberOfPosts] = useState<number>(5);
+  console.log("allPosts", allPosts);
 
   useEffect(() => {
     const getPosts = async () => {
       const response = await fetch(
-        "https://jsonplaceholder.typicode.com/posts?_limit=10"
+        `https://jsonplaceholder.typicode.com/posts?_limit=${numberOfPosts}`
       );
-      const data = await response.json();
-      console.log(data);
+      const data: PostData[] = await response.json();
       setAllPosts(data);
     };
+
     getPosts();
   }, []);
   return (
@@ -21,7 +30,12 @@ const Posts: React.FC = () => {
       <h1>Page Principale</h1>
       <div style={{ display: "flex", flexDirection: "column" }}>
         <label htmlFor="posts">Nombre de publications 5</label>
-        <input type="range" min={1} max={20} />
+        <input
+          type="range"
+          min={1}
+          max={20}
+          onChange={(e) => console.log("target", e.target.value)}
+        />
         <Postslist />
       </div>
     </div>
